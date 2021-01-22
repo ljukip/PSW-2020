8628939cbd1e66b44352aa1a084b3ae795f942af
Ljubica <ljukip1441@gmail.com>
2021-01-22 23:44:21 +0100

    update

---
+++ perscriptionGRPC/src/main/java/com/example/recipe/RequestRecipe.java
@@ -0,0 +1,15 @@
+package com.example.recipe;
+
+public class RequestRecipe {
+
+  private long id;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+}
